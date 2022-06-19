dbaf189a429762af2789433ccf2c2d17bae19d7b
Ayden-spec <62671309+Ayden-spec@users.noreply.github.com>
2022-06-19 15:15:19 +0300

    add button and select component

---
+++ components/ButtonComponent.vue
@@ -0,0 +1,43 @@
+<template>
+  <button @click="onClick" :class="{ active: active, not_active: !active }">
+    {{ placeholder }}
+  </button>
+</template>
+
+<script>
+export default {
+  props: ["placeholder", "active"],
+  methods:{
+    onClick(e){
+      if(this.active){
+        this.$emit('onClick', e)
+      }
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+button {
+  width: 100%;
+  padding: 10px 0;
+  border-radius: 10px;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  font-family: Inter;
+  font-size: 12px;
+  font-weight: 600;
+  text-align: center;
+}
+
+.active {
+  background: $background-green;
+  color: $font-white;
+}
+
+.not_active {
+  background: $background-grey;
+  color: $font-grey;
+}
+</style>
---
+++ components/SelectComponent.vue
@@ -0,0 +1,41 @@
+<template>
+  <select
+    name="roleSelector"
+    id="role_selector"
+    @change="$emit('onInput', $event.target.value)"
+    :value="value"
+  >
+    <option v-for="(element, index) in array" :key="index" :value="element">
+      {{ element }}
+    </option>
+  </select>
+</template>
+
+<script>
+export default {
+  props: ["value", "array"],
+};
+</script>
+
+<style lang="scss" scoped>
+select {
+  border: none;
+  outline: none;
+  padding: 10px;
+  color: $font-grey;
+
+  appearance: none;
+
+  width: 150px;
+  height: 36px;
+
+  background-blend-mode: multiply;
+
+  box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
+  border-radius: 4px;
+  background-size: 5px 5px;
+  background: url("data:image/svg+xml,%3Csvg width='8' height='6' viewBox='0 0 8 6' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M7.48532 1.24264L4.24268 4.48528L1.00003 1.24264' stroke='%23B4B4B4'/%3E%3C/svg%3E")
+    no-repeat right $background-white_two;
+  background-position-x: calc(100% - 10px);
+}
+</style>
